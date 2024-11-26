9fb9d320779efe50a21eb4e7fd76c595fc6b2822
Ольга Коуль <darya.5altykova@yandex.ru>
2024-11-26 20:41:24 +0300

    creating class MYStack based on MYVector

---
+++ Task_8/Task_8/MYStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYStack;
+
+public class MYStack<T>:MYVector.MYVector<T>
+{
+    public void Push(T item) => this.Add(item);
+    
+    public T Pop()
+    {
+        if (this.Empty()) throw new InvalidOperationException("Stack is empty");
+        return this.Remove(this.Size() - 1);
+    }
+
+    public T Peek()
+    {
+        if (this.Empty()) throw new InvalidOperationException("Stack is empty"); 
+        return this.Get(this.Size() - 1);
+    }
+
+    public bool Empty() => this.IsEmpty();
+
+    public int Search (T item)
+    {
+        //if (this.Empty()) throw new InvalidOperationException("Stack is empty");
+        return this.IndexOf(item);
+    }
+
+
+
+
+}
---
+++ Task_8/Task_8/Program.cs
@@ -0,0 +1,5 @@
+﻿//using MYStack;
+//string[] ar = { "jsd", "255", "7821" };
+//MYStack<string> tat = new MYStack<string>();
+//tat.Push(ar[0]);
+//Console.WriteLine(tat.Empty());
