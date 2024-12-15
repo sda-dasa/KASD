2035ae098c5d8a53a51eccd00e921e81db9ba8f1
Ольга Коуль <darya.5altykova@yandex.ru>
2024-12-15 12:30:01 +0300

    Added task № 14 and task № 15. Class MYArrayDeque and Sorting Strings

---
+++ 14_task/14_task/MYArrayDeque.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYDeq
+{
+    internal class MYArrayDeque<E>
+    {
+        E[] elements; byte head; int tail; public int Size { get { return elements.Length; } }
+
+        //public MYArrayDeque(int numElements = 15) { head = 0; tail = numElements; }
+
+        public MYArrayDeque(int numElements = 16) { elements = new E[numElements]; head = 0; tail = 0; }
+
+
+#nullable disable
+        public MYArrayDeque(E[] array)
+        {
+            if (array is null) throw new ArgumentNullException("array");
+            //if (array.Length == 0) return;
+            head = 0; tail = array.Length - 1;
+
+            elements = new E[array.Length];
+
+            for (int i = 0; i < elements.Length; i++) elements[i] = array[i];
+        }
+
+#nullable restore
+
+        public bool IsEmpty(E e) => Size == 0;
+
+
+
+        public void Add(E val)
+        {
+            if (val is null) throw new ArgumentNullException("val");
+            if (tail == elements.Length)
+            {
+                E[] array = new E[(tail * 2) + 1];
+                for (int i = 0; i < tail; i++) array[i] = elements[i];
+                elements = array;
+            }
+            elements[tail++] = val;
+
+        }
+
+        public void AddAll(E[] array)
+        {
+            if (array is null) throw new ArgumentNullException("array");
+            if (tail + array.Length >= elements.Length)
+            {
+                E[] newAr = new E[tail + array.Length + (tail + array.Length) / 2];
+                for (int i = 0; i <= tail - 1; i++) newAr[i] = elements[i];
+                elements = newAr;
+            }
+            for (int i = tail; i < tail + array.Length; i++)
+                elements[i] = array[i - tail];
+            tail += array.Length;
+
+        }
+
+        public bool Contains(object val)
+        {
+            if (val is null) throw new ArgumentNullException("val");
+
+            for (int i = 0; i <= tail; i++)
+            {
+                foreach (E t in elements)
+                {
+                    if (t != null && Equals(t, val)) return true;
+                }
+            }
+            return false;
+        }
+
+
+        public bool ContainsAll(E[] array)
+        {
+            if (array is null) throw new ArgumentNullException("array");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != null && Contains(array[i])) return true;
+            }
+            return false;
+        }
+
+
+        public void Clear(E e) => tail = 0;
+
+
+        public void Remove(E val)
+        {
+            if (Contains(val))
+            {
+                int index = IndexFirstOccurence(val); E[] array = new E[--tail + 1];
+                for (int i = 0; i < index; i++)
+                    array[i] = elements[i];
+
+                for (int i = index + 1; i <= tail; i++)
+                    array[i] = elements[i];
+                elements = array;
+
+            }
+
+        }
+
+
+        public void RemoveAll(E[] array)
+        {
+            if (array is null) throw new ArgumentNullException("array");
+            for (int i = 0; i < array.Length; i++) Remove(array[i]);
+
+        }
+
+
+        public void RetainAll(E[] a)
+        {
+            for (int i = 0; i < a.Length; i++) elements[i] = a[i];
+            tail = a.Length - 1;
+        }
+
+
+
+        private int IndexFirstOccurence(object val) // индекс первого вхождения
+        {
+            if (val is null) throw new ArgumentNullException("val");
+            for (int i = 0; i <= tail; i++)
+                if (val.Equals(elements[i])) return i;
+
+            return -1;
+        }
+
+
+        private int IndexLastOccurence (object val)
+        {
+            if (val is null ) throw new ArgumentNullException("val");
+
+            for (int i = tail; i >= 0; i--)
+                if (val.Equals(elements[i])) return i;
+
+            return -1;
+
+
+        }
+
+        //public void ToArray()
+        //{
+        //    E[] array = new E[tail];
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        array[i] = elements[i];
+        //    }
+        //}
+
+
+        //public E[] ToArray()
+        //{
+        //    E[] newArr = new E[Size]; for (int i = 0; i <  Size; i++) newArr[i] = elements[i];
+        //    return newArr;
+        //}
+
+
+        //public E[] ToArray(E[] arr)
+        //{
+        //    if (arr is null) { E[] newArr = this.ToArray(); return newArr; }
+
+        //    arr = this.ToArray(); return arr;
+
+        //}
+
+
+
+
+
+        //public void ToArray(E[] a)
+        //{
+        //    for (int i = 0; i < tail; i++)
+        //    {
+        //        Add(a[i]);
+        //    }
+        //    tail += a.Length;
+        //    E[] array = new E[tail];
+        //    for (int i = 0; i < array.Length; i++) array[i] = elements[i];
+        //}
+
+
+
+
+        public E[] ToArray()
+        {
+            E[] newAr = new E[Size];
+            for (int i = 0; i < tail; i++) newAr[i] = elements[i];
+            return newAr;
+
+        }
+
+        public void ToArray(ref E[] array)
+        {
+            if (array == null) array = ToArray();
+
+            if (array.Length >= tail + 1)
+                for (int i = 0; i < Size; i++) array[i] = elements[i]; return;
+            throw new ArgumentException("The length of the array is less than the length of the vector");
+
+        }
+
+
+
+        //public E Element() => elements[head];
+
+
+
+        public E Peek()
+        {
+            if (Size == 0) return default(E);
+            return elements[head];
+        }
+
+
+        public E Poll()
+        {
+            if (Size == 0) return default(E);
+            else
+            {
+                E val = elements[head]; Remove(elements[head]);
+                return val;
+            }
+        }
+
+
+
+        public void Push(E val)
+        {
+            if (val is null) throw new ArgumentNullException(nameof(val));
+            E[] array = new E[++tail + 1];
+            array[0] = val;
+            for (int i = 1; i < tail; i++) array[i] = elements[i - 1];
+            elements = array;
+        }
+
+
+        public void AddFirst(E val) => this.Push(val);  
+        
+
+        public void AddLast(E val)
+        {
+            if (val is null) throw new ArgumentNullException("val");
+            Add(val);
+        }
+
+
+        public void ShowEl()
+        {
+            if (Size == 0) return;
+            for (int i = 0; i < Size; i++)
+            {
+                Console.WriteLine(elements[i]);
+            }
+        }
+
+
+
+        public E GetFirst()
+        {
+            if (Size > 0) return elements[head];
+            throw new IndexOutOfRangeException("Deque is empty");
+        }
+
+
+        public override string ToString()
+        {
+            string res = string.Empty;
+            for (int i = 0; i < Size; i++)
+            {
+                res += elements[i] + "\n";
+            }
+            return res;
+        }
+
+
+
+        public bool RemoveFirstOccurrence(object obj)
+        {
+            if (obj is null) throw new ArgumentException("obj");
+            int index = IndexFirstOccurence(obj);
+            if (index > -1)
+            {
+                E[] array = new E[tail--];
+                for (int i = 0; i < index; i++)
+                    array[i] = elements[i];
+                for (int i = index + 1; i <= tail; i++)
+                    array[i] = elements[i];
+                elements = array;
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+        public E GetLast()
+        {
+            if (Size > 0) return elements[tail];
+            throw new IndexOutOfRangeException("Deque is empty");
+        }
+
+
+        private bool Offer(E val)
+        {
+            if (val is null) throw new ArgumentNullException("val");
+            Add(val);
+            if (Contains(val)) return true;
+            else return false;
+        }
+
+#nullable disable
+        public bool OfferFirst(E val)
+        {
+            AddFirst(val); if (Contains(val)) return true;
+            return false;
+        }
+
+
+
+
+        public bool OfferLast(E obj)
+        {
+            AddLast(obj); if (Contains(obj)) return true;
+            return false;
+        }
+#nullable restore
+
+
+        public E Pop()
+        {
+            E val = elements[head];
+            Remove(val);
+            return val;
+        }
+
+
+
+
+        public E PeekFirst()
+        {
+            if (tail == 0) return default(E);
+            else return elements[head];
+        }
+
+
+
+        public E PeekLast()
+        {
+            if (Size == 0) return default(E);
+            else return elements[tail];
+        }
+
+
+        public bool RemoveLastOccurrence(object obj)
+        {
+            if (obj is null) throw new ArgumentException("obj");
+            int index = IndexLastOccurence(obj);
+
+            if (index > -1)
+            {
+                E[] array = new E[tail--];
+                for (int i = 0; i < index; i++)
+                    array[i] = elements[i];
+                for (int i = index + 1; i <= tail; i++)
+                    array[i] = elements[i];
+                elements = array;
+                return true;
+            }
+            return false;
+
+        }
+
+
+
+
+
+        public E PollFirst()
+        {
+            if (Size == 0) return default(E);
+            else
+            {
+                E val = elements[head];
+                Remove(elements[head]);
+                return val;
+            }
+        }
+
+
+        public E PollLast()
+        {
+            if (Size == 0) return default(E);
+            else
+            {
+                E val = elements[tail];
+                Remove(elements[tail]);
+                return val;
+            }
+        }
+
+
+        public E RemoveLast()
+        {
+            if (Size == 0) throw new IndexOutOfRangeException("Deque is empty");
+            E element = elements[tail];
+            Remove(elements[tail]);
+            return element;
+        }
+
+        public E RemoveFirst()
+        {
+            if (Size == 0) throw new IndexOutOfRangeException("Deque is empty");
+            E element = elements[head];
+            Remove(elements[head]);
+            return element;
+        }
+
+
+
+
+
+
+    }
+
+}
+
--- 14_task/14_task/Program.cs
+++ 14_task/14_task/Program.cs
@@ -1,391 +1,96 @@
 ﻿
 using System;
 using System.Drawing;
+using System.IO;
 using System.Reflection.Metadata.Ecma335;
+using MYDeq;
 
 namespace task_14
 {
-    public class MYArrayDeque<E>
+    class Program
     {
-        E[] elements; byte head; int tail; public int Size { get { return elements.Length; } }
 
-        //public MYArrayDeque(int numElements = 15) { head = 0; tail = numElements; }
-
-        public MYArrayDeque (int numElements = 16) {  elements = new E[numElements]; head = 0; tail = 0; }
-
-
-
-        public MYArrayDeque(E[] array)
+        static void Main()
         {
-            if (array.Length == 0) return;
-            head = 0; tail = array.Length - 1;
+            //Console.WriteLine("skdvl");
+            //int n = 9; int[] maqs = new int[n--]; Console.WriteLine(maqs.Length); Console.WriteLine(n);
 
-            elements = new E[array.Length];
+            // чтение из файла
 
-            for (int i = 0; i < elements.Length; i++) elements[i] = array[i];
-        }
+            MYArrayDeque<string> origin = new MYArrayDeque<string>(0);
+
+            string file = "input.txt";
+
+            string[] mas_str = File.ReadAllLines(file);
+            //const int amount = 4;
+            //string[] mas_str = new string [amount]
+            //{ 
+            //    "eeeeyue 89qhacl", 
+            //    "   hslds sdi ksd 9091890980980980980983892032 jsd9q3u9nsof90erqn   q0-q0-0-0- ", 
+            //    "gsdfkjh q",
+            //    " hlsj 83r jzdf 81 8 18 18 1hsf  888812  9190 "
+            //};
 
 
-        public bool IsEmpty(E e) => Size == 0;
-        
-
-
-        public void Add(E el)
-        {
-            if (tail == elements.Length)
+            if (mas_str.Length > 0)
             {
-                E[] array = new E[(tail * 2) + 1];
-                for (int i = 0; i <= tail; i++) array[i] = elements[i];
-                elements = array;
-            }
-            elements[tail++] = el;
-            
-        }
+                origin.Add(mas_str[0]);
 
-        public void AddAll(E[] array)
-        {
-            if (array is null) throw new ArgumentNullException("array");
-            if (tail + array.Length >= elements.Length)
-            {
-                E[] newAr = new E[tail + array.Length + (tail + array.Length) / 2];
-                for (int i = 0; i <= tail  - 1; i++) newAr[i] = elements[i];
-                elements = newAr;
-            }
-            for (int i = tail; i < tail + array.Length; i++)
-                elements[i] = array[i - tail];
-            tail += array.Length;
-
-        }
-        
-        public bool Contains(object val)
-        {
-            if (val is null) throw new ArgumentNullException("val");
-
-            for (int i = 0; i <= tail; i++)
-            {
-                foreach (E t in elements)
+                for (int i = 1; i < mas_str.Length; i++)
                 {
-                    if (t != null && Equals(t, val)) return true;
+                    if (Compare(origin.GetFirst(), mas_str[i]))
+                    { origin.AddLast(mas_str[i]); continue; }
+
+                    origin.AddFirst(mas_str[i]);
+
                 }
+
+                //origin.ShowEl();
+
+                file = "sorted.txt";
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    writer.WriteLine(origin.ToString());
+                }
+
+                Console.WriteLine("Введите кол-во пробелов");
+                int n; int.TryParse(Console.ReadLine(), out n);
+
+                if (!(n == 0 || n < 0))
+                {
+                    string[] mas_str_ = origin.ToArray();
+                    for (int i = 0; i < origin.Size; i++)
+                    {
+                        if (CountOfSpace(mas_str_[i]) > n) origin.Remove(mas_str_[i]);
+                    }
+                }
+
+                origin.ShowEl();
+
+
             }
+            else Console.WriteLine("файл пуст");
+
+        }
+
+        public static byte CountOfSpace (string str)
+        {
+            byte count = 0;
+            foreach (char c in str) if (c == ' ') count++; return count;
+
+        }
+
+        protected static bool Compare(string head, string current)
+        {
+            if (CountOfDigits(head) < CountOfDigits(current)) return true;
             return false;
         }
 
-
-        public bool ContainsAll(E[] array)
+        protected static byte CountOfDigits(string str)
         {
-            if (array is null) throw new ArgumentNullException("array");
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] != null && Contains(array[i])) return true;
-            }
-            return false;
-        }
-        
-
-        public void Clear(E e) => tail = 0;
-
-
-        public void Remove(E val)
-        {
-            if (Contains(val))
-            {
-                int index = FindIndex(val); E[] array = new E[--tail + 1];
-                for (int i = 0; i < index; i++) 
-                    array[i] = elements[i];
-
-                for (int i = index + 1; i <= tail; i++) 
-                    array[i] = elements[i];
-                elements = array;
-
-            }
-
-        }
-
-
-        public void RemoveAll(E[] array)
-        {
-            if (array is null) throw new ArgumentNullException("array");
-            for (int i = 0; i < array.Length; i++) Remove(array[i]);
-
-        }
-
-
-        public void RetainAll(E[] a)
-        {
-            for (int i = 0; i < a.Length; i++) elements[i] = a[i];
-            tail = a.Length - 1;
-        }
-        
-
-
-        private int FindIndex(object val)
-        {
-            if (val is null) throw new ArgumentNullException("val");
-            for (int i = 0; i <= tail; i++)
-                if (val.Equals(elements[i])) return i;
-             
-            return -1;
-        }
-
-
-        //public void ToArray()
-        //{
-        //    E[] array = new E[tail];
-        //    for (int i = 0; i < array.Length; i++)
-        //    {
-        //        array[i] = elements[i];
-        //    }
-        //}
-
-
-        //public E[] ToArray()
-        //{
-        //    E[] newArr = new E[Size]; for (int i = 0; i <  Size; i++) newArr[i] = elements[i];
-        //    return newArr;
-        //}
-
-
-        //public E[] ToArray(E[] arr)
-        //{
-        //    if (arr is null) { E[] newArr = this.ToArray(); return newArr; }
-
-        //    arr = this.ToArray(); return arr;
-
-        //}
-
-
-
-
-
-        //public void ToArray(E[] a)
-        //{
-        //    for (int i = 0; i < tail; i++)
-        //    {
-        //        Add(a[i]);
-        //    }
-        //    tail += a.Length;
-        //    E[] array = new E[tail];
-        //    for (int i = 0; i < array.Length; i++) array[i] = elements[i];
-        //}
-
-
-
-
-        public E[] ToArray()
-        {
-            E[] newAr = new E[Size];
-            for (int i = 0; i <= tail; i++) newAr[i] = elements[i];
-            return newAr;
-
-        }
-
-        public void ToArray(ref E[] array)
-        {
-            if (array == null) array = ToArray();
-
-            if (array.Length >= tail + 1)
-                for (int i = 0; i < Size; i++) array[i] = elements[i]; return;
-            throw new ArgumentException("The length of the array is less than the length of the vector");
-
-        }
-
-
-
-        public E Element() => elements[head];
-        
-
-
-        public E Peek()
-        {
-            if (Size == 0) return default(E);
-            return elements[head];
-        }
-
-
-        public E Poll()
-        {
-            if (Size == 0) return default(E);
-            else
-            {
-                E val = elements[head]; Remove(elements[head]);
-                return val;
-            }
-        }
-
-        public void AddFirst(E val)
-        {
-            E[] array = new E[++tail + 1];
-            array[0] = val;
-            for (int i = 1; i <= tail; i++) array[i] = elements[i];
-            elements = array;
-
-        }
-
-        public void AddLast(E val)
-        {
-            if (val is null) throw new ArgumentNullException("val");
-            Add(val);
-        }
-        
-
-
-
-        public bool Offer(E val)
-        {
-            if (val is null) throw new ArgumentNullException("val");
-            Add(val);
-            if (Contains(val)) return true;
-            else return false;
-        }
-
-
-
-
-        public E GetFirst()
-        {
-            return elements[head];
-        }
-
-
-        public E GetLast()
-        {
-            return elements[tail - 1];
-        }
-
-
-
-        public bool OfferFirst(E obj)
-        {
-            AddFirst(obj);
-            if (Contains(obj)) return true;
-            else return false;
-        }
-
-
-
-
-        public bool OfferLast(E obj)
-        {
-            AddLast(obj);
-            if (Contains(obj)) return true;
-            else return false;
-        }
-
-        public E Pop()
-        {
-            E element = elements[head];
-            Remove(element);
-            return element;
-        }
-
-
-        public void Push(E obj)
-        {
-            E[] array = new E[tail + 1];
-            array[0] = obj;
-            for (int i = 1; i <= tail; i++) array[i] = elements[i];
-            elements = array;
-            tail += 1;
-        }
-
-
-        public E PeekFirst()
-        {
-            if (tail == 0) return default(E);
-            else return elements[head];
-        }
-
-
-
-        public E PeekLast()
-        {
-            if (tail == 0) return default(E);
-            else return elements[tail - 1];
-        }
-
-
-
-
-        public E PollFirst()
-        {
-            if (tail == 0) return default(E);
-            else
-            {
-                E q = elements[head];
-                Remove(elements[head]);
-                return q;
-            }
-        }
-
-
-        public E PollLast()
-        {
-            if (tail == 0) return default(E);
-            else
-            {
-                E q = elements[tail - 1];
-                Remove(elements[tail - 1]);
-                return q;
-            }
-        }
-
-
-        public E RemoveLast()
-        {
-            E element = elements[tail - 1];
-            Remove(elements[tail - 1]);
-            return element;
-        }
-
-        public E RemoveFirst()
-        {
-            E element = elements[head];
-            Remove(elements[head]);
-            return element;
-        }
-        public bool RemoveLastOccurrence(object obj)
-        {
-            int index = -1;
-            for (int i = 0; i < tail; i++) if (obj.Equals(elements[i])) index = i;
-            if (index > -1)
-            {
-                E[] array = new E[tail--];
-                for (int i = 0; i < index; i++) array[i] = elements[i];
-                for (int i = index + 1; i < tail--; i++) array[i] = elements[i];
-                elements = array;
-                tail--;
-                return true;
-            }
-            else return false;
-        }
-        public bool RemoveFirstOccurrence(object obj)
-        {
-            int index = -1;
-            for (int i = 0; i < tail; i++) if (obj.Equals(elements[i])) { index = i; break; }
-            if (index > -1)
-            {
-                E[] array = new E[tail--];
-                for (int i = 0; i < index; i++) array[i] = elements[i];
-                for (int i = index + 1; i < tail--; i++) array[i] = elements[i];
-                elements = array;
-                tail--;
-                return true;
-            }
-            else return false;
-        }
-        public void Print()
-        {
-            Console.WriteLine(this);
-        }
-        public override string ToString()
-        {
-            string s = "";
-            foreach (E t in elements)
-            {
-                s += t + " ";
-            }
-            return s;
+            byte count = 0;
+            foreach (char c in str) if (c >= '0' && c <= '9') count++;
+            return count;
         }
 
     }
