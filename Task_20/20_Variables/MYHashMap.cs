057d87759e575bfecaa8bbd3cde3c12bed319d29
Ольга Коуль <darya.5altykova@yandex.ru>
2025-01-13 19:48:23 +0300

    added task 20

---
+++ Task_20/20_Variables/MYHashMap.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace Class_18
+{
+    internal class Pair<K, E>
+    {
+        public K key; public E entire; public Pair(K key, E entire) { this.key = key; this.entire = entire; }
+    }
+
+    internal class MYHashMap<K, E>
+    {
+        byte size; float loadFactor; public byte Size { get { return size; } }
+
+        MYLinkedList< Pair<K, E> >[] table;
+
+        public MYHashMap(int initialCapacity = 16, float loadFactor = 0.75F) 
+        {
+            table = new MYLinkedList<Pair<K, E>>[initialCapacity];
+
+            for (int i = 0; i < initialCapacity; i++) table[i] = new MYLinkedList<Pair<K, E>>();
+            
+            this.size = 0; this.loadFactor = loadFactor;
+
+        }
+
+        public void Put (K key, E entire)
+        {
+            if (key is null) throw new ArgumentNullException("key");
+
+            Pair<K, E> pair = new(key, entire);
+
+            int index = Math.Abs(key.GetHashCode() % table.Length);
+
+            if (table[index].IsEmpty())
+            {
+                table[index].AddLast(pair);
+            }
+            else
+            {
+                if (table.Length > 0 && (float)size / table.Length >= loadFactor) NewLength();
+
+                int i = 0;
+                foreach (Pair<K, E> p in table[index])
+                {
+                    if (p.key != null && p.key.GetHashCode() == pair.key.GetHashCode())
+                    {
+                        if (p.key.Equals(pair.key))
+                        { table[index].Set(i, pair); break; }
+                    }
+                    i++;
+                }
+                if (i >= table[index].Size - 1) table[index].AddLast(pair);
+
+            }
+
+            size++;
+
+        }
+
+        private void NewLength()
+        {
+            MYLinkedList<Pair<K, E>> [] new_table = new MYLinkedList<Pair<K, E>>[(int) (table.Length * 1.75)];
+
+            for (int i = 0; i < size; i++) new_table[i] = table[i];
+
+            for (int i = size; i < new_table.Length; ++i) new_table[i] = new MYLinkedList<Pair<K, E>>();
+
+            table = new_table;
+
+
+        }
+
+        public void Clear()
+        {
+            table = new MYLinkedList<Pair<K, E>>[table.Length]; size = 0;
+        }
+
+
+        public E? GetEntire (K key)
+        {
+            if (key is null) throw new ArgumentException("key");
+
+            int index = Math.Abs(key.GetHashCode()) % table.Length;
+
+            if (table[index].IsEmpty()) return default;
+
+            foreach (Pair<K, E> p in table[index])
+            {
+                if (p.key != null && p.key.GetHashCode() == key.GetHashCode())
+                {
+                    if (p.key.Equals(key)) return p.entire;
+                }
+            }
+            return default;
+
+
+        }
+
+
+        public bool ContainsKey(object key)
+        {
+            if (key is null) throw new ArgumentException("key");
+
+            int index = Math.Abs(key.GetHashCode()) % table.Length;
+
+            foreach (Pair<K, E> p in table[index])
+            {
+                if (p.key != null && p.key.GetHashCode() == key.GetHashCode())
+                {
+                    if (p.key.Equals(key)) return true; // приведение???
+                }
+            }
+            return false;
+        }
+
+        public bool IsEmpty() => size == 0;
+
+
+        public bool ContainsValue(object value)
+        {
+            if (value is null) throw new ArgumentException(nameof(value));
+
+            if (IsEmpty()) return false;
+
+
+            foreach (MYLinkedList<Pair<K, E>> pair in table)
+            {
+                if (pair.IsEmpty()) continue;
+
+                foreach (Pair<K, E> p in pair)
+                {
+                    if (p.entire != null && p.entire.Equals(value)) return true;
+                }
+
+            }
+
+            return false;
+
+        }
+
+
+        public Pair<K, E>[] EntrySet()
+        {
+            Pair<K, E>[] res = new Pair<K, E>[size];
+            int i = 0;
+            foreach (MYLinkedList<Pair<K, E>> pair in table)
+            {
+                foreach(Pair<K, E> p in pair)
+                {
+                    if (p != null) { res[i] = p; i++; }
+                }
+
+            }
+            return res;
+        }
+
+
+        public K[] KeySet()
+        {
+
+            K [] res = new K[this.size];
+            int i = 0;
+            foreach (MYLinkedList<Pair<K, E>> pair in table)
+            {
+                if (pair.IsEmpty()) continue;
+
+                foreach (Pair<K, E> p in pair)
+                {
+                    if (p != null) { res[i] = p.key; i++; }
+                }
+
+            }
+            return res;
+
+
+        }
+
+
+        public void Remove (object key)
+        {
+
+            if (key is null) throw new ArgumentException("key");
+
+            if (IsEmpty()) throw new Exception("Map is empty");
+
+            int index = Math.Abs(key.GetHashCode()) % table.Length;
+
+            foreach (Pair<K, E> p in table[index])
+            {
+                if (p.key != null && p.key.GetHashCode() == key.GetHashCode())
+                {
+                    if (p.key.Equals(key)) table[index].Remove(p);
+                }
+            }
+            size--;
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Task_20/20_Variables/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Class_18;
+
+
+internal partial class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        StreamReader sR = new StreamReader("source.txt");
+        string example = sR.ReadToEnd();
+
+
+        string rule = @"[A-Za-z0-9]+\s+[a-z]+[A-Z_a-z0-9]*\s*=\s*[0-9]+\s*;";
+
+        //string rule = @"^\s*[A-Za-z0-9]+\s+[a-z]+[A-Z_a-z0-9]*\s*=\s*[0-9]+;";
+
+        Regex cheker = new Regex(rule, RegexOptions.Multiline);
+
+        MatchCollection variants = cheker.Matches(example);
+
+        MYHashMap<string, VariableType> varDefiner = new MYHashMap<string, VariableType>();
+        int i = 0;
+
+
+        string filepath = "result.txt";
+        StreamWriter sWrt = new StreamWriter(filepath);
+
+
+        foreach (Match variant in variants)
+        {
+            if (variant.Length > 6)
+            {
+                VariableType curType = RightType(variant.Value);
+                if (!(curType is VariableType.NotStated))
+                {
+                    string varName = GetVarName(variant.Value);
+
+                    if (!varDefiner.IsEmpty() && varDefiner.ContainsKey(varName))
+                        // записываем переопределение в файл
+                        sWrt.WriteLine($"{ToString(curType)} => " +
+                            $"{varName}");
+                    else
+                        varDefiner.Put(varName, curType);
+                }
+                else Console.WriteLine("некорректное определение - " +
+                    $"не соответствующий тип переменной (строка файла - {i})");
+                 
+            }
+            i++;
+
+        }
+
+        sWrt.Close();
+
+
+
+    }
+
+
+    static string GetVarName(string wholestr)
+    {
+        if (wholestr.Length == 0) throw new ArgumentException(nameof(wholestr));
+
+        string varName = string.Empty; int i = wholestr.IndexOf(" ");
+        while (wholestr[i] == ' ') i++;
+
+        while (wholestr[i] != '=' && wholestr[i]!= ' ') { varName += wholestr[i]; i++; }
+
+        while (wholestr[i] == ' ') i++;
+        if (wholestr[i] != '=') return string.Empty;
+
+
+        string rule = @"[a-z]+[A-Za-z0-9]*";
+
+        Regex cheker = new Regex(rule);
+
+        MatchCollection res = cheker.Matches(varName);
+
+        if (res.Count > 0) return varName;
+
+        return string.Empty;
+
+
+    }
+
+    static string ToString (VariableType type)
+    {
+        switch (type)
+        {
+            case VariableType.Int: return "int";
+            case VariableType.Double: return "double";
+            case VariableType.Float: return "float";
+            default: return string.Empty;
+        }
+    }
+
+
+    static VariableType RightType(string v)
+    {
+        if (string.Format($"{v[0]}{v[1]}{v[2]}").Equals("int")) return VariableType.Int;
+
+        if (string.Format($"{v[0]}{v[1]}{v[2]}{v[3]}{v[4]}").Equals("float")) return VariableType.Float;
+
+        if (string.Format($"{v[0]}{v[1]}{v[2]}{v[3]}{v[4]}{v[5]}").Equals("double")) return VariableType.Double;
+
+        return VariableType.NotStated;
+
+    }
+
+
+
+    enum VariableType : int
+    {
+        Int,
+        Double,
+        Float,
+        NotStated = -1
+
+    };
+
+
+
+
+
+
+}
